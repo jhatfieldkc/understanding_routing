d73fcbce6e967f0a1a0de2d06950c4910bd3cc88
Jeremy Hatfield <jhatfieldkc@gmail.com>
2021-06-11 12:11:57 -0500

    updated error handling

--- understanding.py
+++ understanding.py
@@ -4,20 +4,25 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+    
 @app.route('/dojo')
 def dojo():
     return 'Dojo!'
+
 @app.route('/say/<string:name>')
 def hello(name):
     print(name)
     return "Hello, " + name
+
 @app.route('/repeat/<int:num>/<word>')
 def repeat(num, word):
     print(word)
     return word * num
-@app.route('/<path:path>')
-def catch_all(path):
-    return 'Sorry!  No response.  Try again.'
+
+@app.errorhandler(404)
+def catch_all(err):
+    print(type(err))
+    return "Sorry its broken"
 
 if __name__=="__main__":
     app.run(debug=True)
